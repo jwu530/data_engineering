998ffe4471b349cf85f4e3b7c1d67a6db7fcb840
jwu530 <wenchie1@asu.edu>
2022-03-20 20:58:56 -0700

    first

---
+++ python_method/get_method.py
@@ -0,0 +1,13 @@
+
+# Python dictionary method
+# The get() method returns the value of the item with the specified key.
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.get("model")
+
+print(x)
